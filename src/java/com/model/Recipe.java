07aa41ed2f4638ae96dafbecbad470910fb86812
S63606 <s63606@ocean.umt.edu.my>
2024-06-23 13:05:42 +0800

    Added Recipe model/Bean.

---
+++ src/java/com/model/Recipe.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.model;
+
+/**
+ *
+ * @author saifu
+ */
+public class Recipe {
+    private String title;
+    private String content;
+
+    public Recipe() {
+    }
+
+    public Recipe(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    
+}
