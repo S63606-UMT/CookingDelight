b65631e1bd3c89dc16ae5d385db14893ab9be3a7
S63606 <s63606@ocean.umt.edu.my>
2024-06-24 19:05:22 +0800

    updated database records.

--- sql/cooking_delight_db.sql
+++ sql/cooking_delight_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2024 at 03:54 PM
+-- Generation Time: Jun 24, 2024 at 01:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,28 +24,94 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `username` varchar(100) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `recipeid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletter`
+--
+
+CREATE TABLE `newsletter` (
+  `email` varchar(100) NOT NULL,
+  `created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `newsletter`
+--
+
+INSERT INTO `newsletter` (`email`, `created`) VALUES
+('s63606@ocean.umt.edu.my', '2024-06-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe`
+--
+
+CREATE TABLE `recipe` (
+  `recipeid` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `shortDescription` varchar(200) NOT NULL,
+  `prepTime` int(11) NOT NULL,
+  `cookTime` int(11) NOT NULL,
+  `serving` int(11) NOT NULL,
+  `ingredients` varchar(300) NOT NULL,
+  `instructions` varchar(1000) NOT NULL,
+  `chefNote` varchar(150) DEFAULT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `userid` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(75) NOT NULL,
   `email` varchar(50) NOT NULL,
   `dateOfBirth` date NOT NULL,
-  `gender` varchar(10) NOT NULL
+  `gender` varchar(10) NOT NULL,
+  `description` varchar(310) DEFAULT NULL,
+  `picturePath` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userid`, `username`, `password`, `email`, `dateOfBirth`, `gender`) VALUES
-(1, 'Test', 'tesy', 'test@gmail.com', '2024-06-01', 'male');
+INSERT INTO `users` (`userid`, `username`, `password`, `email`, `dateOfBirth`, `gender`, `description`, `picturePath`) VALUES
+(9, 'brad', '$2a$10$s9aiqdqeDFkgglyyBO6ohe7WGU/CeZDBDEBH.hA5xOO6evf8OgpHS', 'BradPitt@gmail.com', '2024-06-01', 'male', 'A. B.\r\nC.', NULL),
+(10, 'test', '$2a$10$Ws8fHgf7Xn2dxb2ycRoPTuUdGodQauN2NxdOQN2ZI6dYgVGs45I.6', 'test@gmail.com', '2024-06-01', 'male', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD KEY `comment_recipeid` (`recipeid`);
+
+--
+-- Indexes for table `recipe`
+--
+ALTER TABLE `recipe`
+  ADD PRIMARY KEY (`recipeid`),
+  ADD KEY `fk_userid` (`userid`);
 
 --
 -- Indexes for table `users`
@@ -58,10 +124,32 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `recipe`
+--
+ALTER TABLE `recipe`
+  MODIFY `recipeid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_recipeid` FOREIGN KEY (`recipeid`) REFERENCES `recipe` (`recipeid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `recipe`
+--
+ALTER TABLE `recipe`
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
